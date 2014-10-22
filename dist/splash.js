563f45ae5a3fa1929e98adf7a9a059146cea9f25
Howard.Zuo <leftstick@qq.com>
2014-10-22 09:42:46 +0800

    finish code

---
+++ dist/splash.js
@@ -0,0 +1,103 @@
+/**
+ * splash-screen is very simple to use to setup a splash-screen for your
+ * application
+ *
+ * @author Howard.Zuo
+ * @date   Sep 22th, 2014
+ *
+ **/
+(function(global, factory) {
+
+    'use strict';
+
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else {
+        global.Splash = factory();
+    }
+
+}(this, function() {
+    'use strict';
+
+    var Splash = {};
+    Splash.version = '1.0.0';
+
+    var hasClass = function(ele, cls) {
+        return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+    };
+
+    var addClass = function(ele, cls) {
+        if (!hasClass(ele, cls)) {
+            ele.className += ' ' + cls;
+        }
+    };
+
+    var removeClass = function(ele, cls) {
+        if (hasClass(ele, cls)) {
+            var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+            ele.className = ele.className.replace(reg, ' ');
+        }
+    };
+
+    var $html = document.getElementsByTagName('html')[0];
+    var $body;
+
+    var docWidth = document.documentElement.clientWidth;
+    var docHeight = document.documentElement.clientHeight;
+
+    var $splash;
+
+    var setSplash = function() {
+        addClass($html, 'splash');
+        var loadDiv = document.createElement('div');
+        loadDiv.setAttribute('class', 'load');
+        loadDiv.innerText = 'Loading';
+        var chargeDiv = document.createElement('div');
+        chargeDiv.setAttribute('class', 'charge');
+
+        var progressDiv = document.createElement('div');
+        progressDiv.setAttribute('class', 'progress');
+        progressDiv.appendChild(loadDiv);
+        progressDiv.appendChild(chargeDiv);
+        progressDiv.style.width = docWidth / 2 + 'px';
+        progressDiv.style.left = docWidth / 4 + 'px';
+
+        $splash = document.createElement('div');
+        $splash.setAttribute('class', 'splash-screen');
+        $splash.appendChild(progressDiv);
+
+        $body.appendChild($splash);
+    };
+
+    var reloadBody = function() {
+        setTimeout(function() {
+            $body = document.body;
+            if (!$body) {
+                reloadBody();
+                return;
+            }
+            setSplash();
+        }, 100);
+    };
+
+    Splash.enable = function() {
+        $body = document.body;
+        if (!$body) {
+            reloadBody();
+            return;
+        }
+        setSplash();
+    };
+
+    Splash.destroy = function() {
+        removeClass($html, 'splash');
+        if ($body && $splash) {
+            $body.removeChild($splash);
+        }
+    };
+
+    return Splash;
+
+}));
--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@ var less = function(dest, isProduction) {
     var gulp = require('gulp');
     var less = require('gulp-less');
     var prefix = require('gulp-autoprefixer');
+    var rename = require('gulp-rename');
     var sourcemap = require('gulp-sourcemaps');
     return gulp.src('src/splash.less')
         .pipe(sourcemap.init())
+        .pipe(rename({
+            basename: isProduction ? 'splash.min' : 'splash'
+        }))
         .pipe(less({
             compress: isProduction
         }))
@@ -20,8 +24,12 @@ var less = function(dest, isProduction) {
         .pipe(gulp.dest(dest));
 };
 
-gulp.task('lessdemo', function() {
+gulp.task('lessDemo', function() {
     return less('demo/', false);
+});
+
+gulp.task('lessDist', function() {
+    return less('dist/', true);
 });
 
 gulp.task('copyDemo', function() {
@@ -29,7 +37,12 @@ gulp.task('copyDemo', function() {
         .pipe(gulp.dest('demo/'));
 });
 
-gulp.task('demo', ['lessdemo', 'copyDemo'], function() {
+gulp.task('copyDist', function() {
+    return gulp.src('./src/splash.js')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('demo', ['lessDemo', 'copyDemo'], function() {
     var webserver = require('gulp-webserver');
     gulp.src('demo/')
         .pipe(webserver({
@@ -39,4 +52,22 @@ gulp.task('demo', ['lessdemo', 'copyDemo'], function() {
             directoryListing: false,
             fallback: 'index.html'
         }));
+});
+
+
+gulp.task('dist', ['lessDist', 'copyDist'], function() {
+    var uglify = require('gulp-uglify');
+    var sourcemaps = require('gulp-sourcemaps');
+    var rename = require('gulp-rename');
+
+    return gulp.src('./dist/splash.js')
+        .pipe(sourcemaps.init())
+        .pipe(rename({
+            basename: 'splash.min'
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./', {
+            sourceRoot: '.'
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
