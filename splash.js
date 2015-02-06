8359ecdba3858402d5a1b1b5c16fc45d1876bea0
Howard.Zuo <leftstick@qq.com>
2015-02-06 10:52:48 +0800

    update demo

--- splash.js
+++ splash.js
@@ -3,7 +3,7 @@
  * application
  *
  * @author Howard.Zuo
- * @date   Feb 5th, 2015
+ * @date   Feb 6th, 2015
  *
  **/
 (function(global, factory) {
@@ -22,27 +22,62 @@
     'use strict';
 
     var Splash = {};
-    Splash.version = '2.0.0';
+    Splash.version = '2.1.0';
+
+    var elementClass = function(tag, className) {
+        var ele = document.createElement(tag);
+        ele.setAttribute('class', className);
+        return ele;
+    };
+
+    var elementTxt = function(tag, text) {
+        var ele = document.createElement(tag);
+        ele.innerText = text;
+        return ele;
+    };
 
     var splashDiv = function() {
-        var splash = document.createElement('div');
-        splash.setAttribute('class', 'splash');
-        return splash;
+        return elementClass('div', 'splash');
     };
 
     var tailingHandler = function($splash) {
-        var loading = document.createElement('span');
-        loading.innerText = 'Loading';
-        $splash.appendChild(loading);
+        $splash.appendChild(elementTxt('span', 'Loading'));
     };
 
     var windcatcherHandler = function($splash) {
-        var windcatcher;
         for (var i = 0; i < 8; i++) {
-            windcatcher = document.createElement('div');
-            windcatcher.setAttribute('class', 'blade');
-            $splash.appendChild(windcatcher);
+            $splash.appendChild(elementClass('div', 'blade'));
         }
+    };
+
+    var circularHandler = function($splash) {
+
+        var arr = ['spinner-blue', 'spinner-red', 'spinner-yellow', 'spinner-green'];
+
+        for (var i = 0; i < arr.length; i++) {
+            var layer = elementClass('div', 'spinner-layer ' + arr[i]);
+
+            var circleLeft = elementClass('div', 'circle-clipper left');
+            var circle01 = elementClass('div', 'circle');
+
+            circleLeft.appendChild(circle01);
+            layer.appendChild(circleLeft);
+
+            var gapPatch = elementClass('div', 'gap-patch');
+            var circle02 = elementClass('div', 'circle');
+
+            gapPatch.appendChild(circle02);
+            layer.appendChild(gapPatch);
+
+            var circleRight = elementClass('div', 'circle-clipper right');
+            var circle03 = elementClass('div', 'circle');
+            circleRight.appendChild(circle03);
+
+            layer.appendChild(circleRight);
+
+            $splash.appendChild(layer);
+        }
+
     };
 
     var emptyHandler = function() {};
@@ -52,7 +87,8 @@
         windcatcher: windcatcherHandler,
         'audio-wave': emptyHandler,
         'spinner-section': emptyHandler,
-        'spinner-section-far': emptyHandler
+        'spinner-section-far': emptyHandler,
+        circular: circularHandler
     };
 
     var hasClass = function(ele, cls) {
